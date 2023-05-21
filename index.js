0245b55f351c807df073ba2c6f718b02816cbbf8
Marion E Smith <titleofliberty@users.noreply.github.com>
2023-05-20 21:28:23 -0400

    backup

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const jetpack = require("fs-jetpack");
 const moment = require("moment/moment");
+const { DiceRoll } = require("@dice-roller/rpg-dice-roller");
+
 var win = nw.Window.get();
+
+$(document).on("keyup", function(event) {
+
+    if (event.code == "Digit4") {
+        var roll = new DiceRoll("1d4");
+        $("#output").append("<div>" + roll.output + "</div>");
+    }
+});
